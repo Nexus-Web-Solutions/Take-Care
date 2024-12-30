71a414b23315d1090db62dcb5d6aeaee8df024f4
StDensity <noyaljosephnnj@gmail.com>
2024-12-30 14:34:22 +0530

    feat: add Gallery component to About page with image collage and animations

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import About from "./pages/About";
+import { Gallery } from "./components/pages/about/Gallery";
+// import About from "./pages/About";
 
 createRoot(document.getElementById("root")).render(
    <StrictMode>
       <Navbar />
-      <About />
-
+      {/* <About /> */}
+<Gallery />
       <Footer />
    </StrictMode>
 );
