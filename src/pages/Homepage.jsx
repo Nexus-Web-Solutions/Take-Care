5768a1dd290b44b28ade66fb323c225d6a6d7b09
StDensity <noyaljosephnnj@gmail.com>
2025-01-02 12:00:37 +0530

    refactor: Moved the collage component path to global folder and updated import paths for Collage component in Gallery and Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import Hero from '../components/pages/home_page/Hero'
-import About from '../components/pages/home_page/About'
-import Services from '../components/pages/home_page/Services'
-import Contact from '../components/pages/home_page/Contact'
-import Collage from '@/components/pages/about/Collage'
+import React from "react";
+import Hero from "../components/pages/home_page/Hero";
+import About from "../components/pages/home_page/About";
+import Services from "../components/pages/home_page/Services";
+import Contact from "../components/pages/home_page/Contact";
+import Collage from "@/components/pages/global/Collage";
 
 const Homepage = () => {
-  return (
-    <div>
-    <section id='home'>
-        <Hero/>
-      </section>
-      <section id='About_US'>
-        <Collage bgColor="white" />
-      </section>
-      <section id='Services'>
-        <Services/>
-      </section>
-      <section id='contact'>
-        <Contact/>
-      </section>
-    </div>
-  )
-}
+   return (
+      <div>
+         <section id="home">
+            <Hero />
+         </section>
+         <section id="About_US">
+            <Collage bgColor="white" />
+         </section>
+         <section id="Services">
+            <Services />
+         </section>
+         <section id="contact">
+            <Contact />
+         </section>
+      </div>
+   );
+};
 
-export default Homepage
+export default Homepage;
