01ce113b605cbca1618c1d93291d8e17bc655484
StDensity <noyaljosephnnj@gmail.com>
2025-01-03 11:33:28 +0530

    refactor: remove unused About component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Hero from "../components/pages/home_page/Hero";
-import About from "../components/pages/home_page/About";
 import Services from "../components/pages/home_page/Services";
 import Contact from "../components/pages/home_page/Contact";
 import Collage from "@/components/pages/global/Collage";
