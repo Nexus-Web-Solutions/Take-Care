4a41c18aa3645a68271fb3b02370cd7548e2b9ab
StDensity <noyaljosephnnj@gmail.com>
2025-01-04 13:44:33 +0530

    refactor: improve layout and spacing in ServicesInView component

--- src/components/pages/services/ServicesInView.jsx
+++ src/components/pages/services/ServicesInView.jsx
@@ -21,17 +21,17 @@ const ServicesInView = ({ title, description, image, imgLeft }) => {
          transition={{ duration: 0.3, ease: "easeInOut" }}
          viewOptions={{ margin: "0px 0px -350px 0px" }}
       >
-         <div className="p-4 grid grid-cols-1 lg:grid-cols-2 bg-take_care_bg gap-8">
-            <div className={`${imgLeft ? "order-1" : "order-2"} flex items-center`}>
+         <div className="p-4 grid grid-cols-1 lg:grid-cols-2 bg-take_care_bg lg:gap-14 gap-3">
+            <div className={`${imgLeft ? "order-1" : "order-2"} flex `}>
                <img
                   src={image}
                   alt="Athletics"
                   className="xl:max-w-[900px] w-full rounded-lg"
                />
             </div>
-            <div className={`${imgLeft ? "order-2" : "order-1"} flex items-center`}>
+            <div className={`${imgLeft ? "order-2" : "order-1"} flex`}>
                <div>
-                  <p className="font-medium text-take_care_primary lg:text-5xl text-4xl text-left">
+                  <p className="font-medium text-take_care_primary lg:pb-10 pb-3 lg:text-5xl text-4xl text-left">
                      {title}
                   </p>
                   <p className="text-sm lg:text-3xl text-left text-zinc-600">
